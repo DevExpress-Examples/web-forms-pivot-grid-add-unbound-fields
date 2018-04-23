8982084ecd98ceaec29814f1e9842baa0a32dcb1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ASPxPivotGrid_CalculateUnboundData/Default.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.UI;
+using DevExpress.Web.ASPxPivotGrid;
+
+namespace AddUnboundField {
+    public partial class _Default : Page {
+        protected void Page_Load(object sender, EventArgs e) {
+        }
+        protected void CustomUnboundFieldData(object sender, CustomFieldDataEventArgs e) {
+            if (e.Field.FieldName != "PriceWithDiscount") return;
+            decimal extPrice = Convert.ToDecimal(e.GetListSourceColumnValue("ExtendedPrice"));
+            decimal discount = Convert.ToDecimal(e.GetListSourceColumnValue("Discount"));
+            e.Value = extPrice * (1 - discount);
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to Add Unbound Fields
+
+
+<p>The following example shows how to add unbound fields to the ASPxPivotGrid.</p>
+<p>The 'DiscountAmount' unbound field's values are calculated using the PivotGridFieldBase.UnboundExpression property, while the 'PriceWithDiscount' unbound field is populated by handling the ASPxPivotGrid.CustomUnboundFieldData event.<br /><br /><b>See Also:</b> <br /><a href="https://www.devexpress.com/Support/Center/p/Q285890"> How to calculate custom values based on other field values</a> <br /><a href="https://documentation.devexpress.com/#AspNet/CustomDocument7259">Unbound Data</a></p>
+
+<br/>
+
+
